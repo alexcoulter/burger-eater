31e322d58831726a68394c8911846bcd46ef9d24
Alex Coulter <ALEXCOULTER.UNC@GMAIL.COM>
2020-03-30 18:23:10 -0400

    changed port

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const mysql = require("mysql");
 
 const connection = mysql.createConnection({
   host: "localhost",
-  port: 3306,
+  port: "3306",
   user: "root",
   password: "newpass",
   database: "burgers_db"
